b9558a3e83d666cd0209a4dcacc1688a93d07388
GHNERCH <166847742+GHNERCH@users.noreply.github.com>
2025-10-10 16:45:37 +0800

    openlist

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -56,8 +56,8 @@ UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 #UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
 
 #UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-
-#UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
+#https://github.com/OpenListTeam/OpenList
+UPDATE_PACKAGE "openlist" "OpenListTeam/OpenList" "main"
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
